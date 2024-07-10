fff0569a696c9d3f1014637f0f1713bc19f47ce1
zhopto3 <zhopton@gmail.com>
2024-07-10 16:34:08 +0200

    Freeze CNN weights

--- training/xlsr_asr.py
+++ training/xlsr_asr.py
@@ -2,7 +2,7 @@ import torch
 import torchaudio
 
 class XLSR_ASR(torch.nn.Module):
-    def __init__(self,O:int, model: str="XLSR_300M"):
+    def __init__(self,O:int, model: str="XLSR_300M", freeze_CNN: bool=True):
         """Provide name of XLS_r model (should be available in torchaudio) and the size of the output layer (vocab size)"""
         super(XLSR_ASR,self).__init__()
 
@@ -18,6 +18,10 @@ class XLSR_ASR(torch.nn.Module):
             raise Exception('Model not found')
 
         self.encoder = self.bundle.get_model()
+        if freeze_CNN:
+            for p in self.encoder.named_parameters():
+                if "feature_extractor" in p[0]:
+                    p[1].requires_grad=False
         self.fc = torch.nn.Linear(self.bundle._params["encoder_embed_dim"],O)
 
     def forward(self,x,lengths):
