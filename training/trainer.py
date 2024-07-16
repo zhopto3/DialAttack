bb6e4a0c33b167fdb683bd7ad29ac6185456960d
zhopto3 <zhopton@gmail.com>
2024-07-16 09:12:09 +0200

    Implement logging

--- training/trainer.py
+++ training/trainer.py
@@ -1,5 +1,6 @@
 from collections import deque
 import os
+import logging
 
 import torch
 
@@ -23,10 +24,16 @@ class Trainer:
 
         self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    def train(self, initial_lr, es_patience, lr_patience):
+    def train(self, initial_lr, es_patience, lr_patience, logger_level = logging.INFO):
+
+        logging.basicConfig(filename=f'{self.name}.log',filemode="w", format='%(asctime)s %(message)s')
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logger_level)
+
         if self.task=="asr":
             self.optimizer = torch.optim.Adam(self.network.parameters(),lr = initial_lr)
             self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer,patience=lr_patience)
+            self.logger.info(f"ASR training:")
             self._train_asr(es_patience)
         elif self.task=="dial_class":
             #@TODO implement optimizer and scheduler for dialect classification if necessary
@@ -40,6 +47,9 @@ class Trainer:
         self._initialize_metrics(patience)
 
         while self._terminate(patience):
+            self.logger.info(f"Epoch {len(self.train_loss_epoch)+1}:")
+            self.logger.info(f"Learning Rate: {self.scheduler.get_last_lr()}")
+            self.logger.info("Train Step\tBatch Loss")
             self.network.train()
             for x,t,_,audio_l,txt_l in self.train_dl:
                 x = x.to(self.device)
@@ -61,11 +71,18 @@ class Trainer:
                 #Update running batch loss & dict
                 self.running_train_loss+=J.item()
                 self.train_loss_batch[len(self.train_loss_batch)]=J.item()
+                self.logger.info(f"{len(self.train_loss_batch)}\t{J.item()}")
+            self.logger.info(f"End of Epoch {len(self.train_loss_epoch)+1}")
             #Update the train epoch loss; reset running train loss to 0
             self.train_loss_epoch[len(self.train_loss_batch)]=self.running_train_loss/len(self.train_dl)
+            self.logger.info(f"Average Train loss over epoch:\t{self.train_loss_epoch[len(self.train_loss_batch)+1]}")
             self.running_train_loss=0.
             #Run validation and update relevant metrics
             J_val = self._vaildation_step_asr()
+            self.logger.info(f"Average Validation loss over epoch:\t{J_val}")
+            self.logger.info(f"Best Validation Epoch: {self.best_val_epoch}")
+            self.logger.info(f"Best Validation Loss: {self.best_val_loss}")
+            self.logger.info(f"Delta Validation loss: {self.delta_queue[-1]}")
             #Adjust learning rate
             self.scheduler.step(J_val)
 
@@ -94,7 +111,7 @@ class Trainer:
         #Update the delta queue
         self.delta_queue.append(self.best_val_loss-prior_best)
         return J_val
-    
+        
     def _train_dial_class(self,optimizer,patience,lr_scheduler):
         #@TODO: If necessary, implement this
         raise Exception("Task not implemented")
@@ -116,7 +133,7 @@ class Trainer:
 
         #queue of length of patience; store the change in the best validation loss after each epoch
         self.delta_queue = deque(maxlen=patience)
-
+    
     def _terminate(self,patience):
         #Keep going until all the items in the queue are 0 (x epochs with no change in best validation loss)
         return False if (any(self.delta_queue)&len(self.delta_queue)==patience) else True
