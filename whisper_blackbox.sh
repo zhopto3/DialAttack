01432fdcf670480b530428fec0d7c8d39ed0384c
zhopto3 <zhopton@gmail.com>
2024-08-23 12:04:43 +0200

    Scripts for testing adversarial on Whisper

---
+++ whisper_blackbox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for experiment in central100_53m_v02 central80_53m_v01 central50_53m_v01 central20_53m_v01
+do
+    mkdir ./experiments/${experiment}/whisper_robust
+    for model in tiny small medium large-v2
+    do
+        python3 ./attack/blackbox_whisper.py --experiment $experiment --model $model > ./experiments/${experiment}/whisper_robust/${model}_blackbox.tsv
+    done
+done
